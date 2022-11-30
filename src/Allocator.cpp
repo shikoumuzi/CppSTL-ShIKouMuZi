8695c7f461a7c739e08d84a55030b97f18d5a503
shikoumuzi <1013740747@qq.com>
2022-12-01 01:54:54 +0800

    提交了内存池管理的一种情况

--- src/Allocator.cpp
+++ src/Allocator.cpp
@@ -1,6 +1,42 @@
 #include"Allocator.h"
 
 namespace MUZI {
+
+	MAllocator::MAllocator()
+	{
+		// 采用引用计数 确保所有程序退出后 内存得到释放
+		MAllocator::object_num += 1;
+		//atexit(MAllocator::callCMF);
+	}
+	MAllocator::~MAllocator()
+	{
+		MAllocator::object_num -= 1;
+		if (MAllocator::object_num == 0)
+		{
+			MAllocator::callCMF();
+		}
+	}
+	void MAllocator::callCMF()
+	{
+		for (int i = 0; i < __MUZI_ALLOCATOR_MOD_SIZE__; ++i)
+		{
+			if(MAllocator::cmf[i] != nullptr)
+				MAllocator::cmf[i](MAllocator::cmf_arg[i]);
+		}
+	}
+	void MAllocator::setClearMemoryFunction(clearMemoryFunction fun, void* arg)
+	{
+		for (int i = 0; i < __MUZI_ALLOCATOR_MOD_SIZE__; ++i)
+		{
+			if (MAllocator::cmf[i] == nullptr)
+			{
+				MAllocator::cmf[i] = fun;
+				MAllocator::cmf_arg[i] = arg;
+				break;
+			}
+		}
+	}
+
 #ifdef __MUZI_ALLOCATOR_MOD_POOL__
 	size_t MAllocator::object_num = 0;
 	size_t MAllocator::pool_mem_total = 0;
@@ -8,21 +44,31 @@ namespace MUZI {
 	union MAllocator::MAllocatorRep* MAllocator::pool_start_free_pool_ptr = nullptr;
 	union MAllocator::MAllocatorRep* MAllocator::pool_end_free_pool_ptr = nullptr;
 	MAllocator::clearMemoryFunction MAllocator::cmf[__MUZI_ALLOCATOR_MOD_SIZE__] = { nullptr };
-	void* MAllocator::cmf_arg = nullptr;
+	void* MAllocator::cmf_arg[__MUZI_ALLOCATOR_MOD_SIZE__] = { nullptr };
 
-	inline size_t MAllocator::pool_RoundUp()
+	inline size_t MAllocator::pool_RoundUp(size_t bytes)
 	{
 		return 
-			(__MUZI_ALLOCAOTR_MOD_POOL_ROUNDUP__(MAllocator::pool_mem_total) + __MUZI_ALLOCAOTR_MOD_POOL_ALIGN__ - 1) 
+			(bytes + __MUZI_ALLOCAOTR_MOD_POOL_ALIGN__ - 1)
 			& -(__MUZI_ALLOCAOTR_MOD_POOL_ALIGN__ - 1);
 	}
 	inline size_t MAllocator::pool_freelist_index(size_t bytes)
 	{
 		return (bytes + __MUZI_ALLOCAOTR_MOD_POOL_ALIGN__ - 1) / __MUZI_ALLOCAOTR_MOD_POOL_ALIGN__ - 1;
 	}
+	inline bool MAllocator::pool_is_possible_mem_board(void* p)
+	{
+		return (*((char*)p) == __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__) ? true : false;
+	}
 
 	void* MAllocator::pool_allocate(size_t type_size)
 	{
+		// 大于上界 即转交给malloc去申请 此时malloc所多出的块相比于数据本身已经很小
+		if (type_size > (size_t)__MUZI_ALLOCAOTR_MOD_POOL_MAX_SPECIFICATION__)
+		{
+			return ::operator new(type_size);
+		}
+
 		MAllocatorRep* ret_mem_ptr = nullptr;
 		size_t pool_index = MAllocator::pool_freelist_index(type_size); // 由大小（八的倍数）获取其坐标
 		size_t pool_index_specification = (pool_index + 1) * __MUZI_ALLOCAOTR_MOD_POOL_ALIGN__; // 获取当前位置对应的内存申请规格
@@ -32,7 +78,7 @@ namespace MUZI {
 		// 发现该点之前未分配或者该点之前分配的分配完了 
 		if (MAllocator::pool_mem_pool[pool_index] == nullptr)
 		{
-			// 并且战备池已经空了
+			// 并且战备池已经空了或者从未申请
 			if (MAllocator::pool_start_free_pool_ptr == MAllocator::pool_end_free_pool_ptr)
 			{
 				// 一半用以正常申请内容 一半用以战备池
@@ -51,6 +97,7 @@ namespace MUZI {
 				MAllocatorRep* list_next_ptr =
 					(MAllocatorRep*)((char*)MAllocator::pool_start_free_pool_ptr - pool_index_specification);
 				list_next_ptr->next = nullptr;
+				list_next_ptr->data[0] |= __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__;// 添加内存标识符
 				MAllocatorRep* list_next_ptr_temp = list_next_ptr;
 
 				// 构建空闲单向链表来切割内存
@@ -59,6 +106,7 @@ namespace MUZI {
 					list_next_ptr =
 						(MAllocatorRep*)((char*)list_next_ptr - pool_index_specification);
 					list_next_ptr->next = list_next_ptr_temp;
+					list_next_ptr->data[0] |= __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__;// 添加内存标识符
 					list_next_ptr_temp = list_next_ptr;
 				}
 
@@ -67,10 +115,32 @@ namespace MUZI {
 			}
 			else// 战备池有剩余
 			{
-				size_t pool_size = MAllocator::pool_end_free_pool_ptr - MAllocator::pool_start_free_pool_ptr;
-				if (type_size > pool_size)
+				size_t pool_size = 
+					(MAllocator::pool_end_free_pool_ptr - MAllocator::pool_start_free_pool_ptr) / 8;
+				if (type_size <= pool_size)
 				{
-					size_t pool_block_count = pool_size / type_size;
+					size_t pool_block_count = pool_size / type_size;// 可以分割多少个内存块
+					size_t residue_mem = pool_size % type_size;// 剩余内存
+					MAllocator::pool_mem_pool[pool_index] = MAllocator::pool_start_free_pool_ptr;
+					// 分割内存 从头往后分割
+					MAllocatorRep* list_next_ptr =
+						static_cast<MAllocatorRep*>(MAllocator::pool_start_free_pool_ptr);
+					MAllocatorRep* list_next_ptr_temp = list_next_ptr;// 临时指针
+					for (int i = 0; i < pool_block_count; ++i)
+					{
+						list_next_ptr_temp =
+							(MAllocatorRep*)((char*)list_next_ptr + pool_index_specification);
+						list_next_ptr->next = list_next_ptr_temp;
+						list_next_ptr->data[0] |= __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__;// 添加内存标识符
+						list_next_ptr = list_next_ptr_temp;
+					}
+
+					list_next_ptr->next = nullptr;
+					list_next_ptr->data[0] |= __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__;// 添加内存标识符
+				}
+				else
+				{
+
 				}
 			}
 		}
@@ -83,28 +153,26 @@ namespace MUZI {
 
 		}
 
+		if(ret_mem_ptr != nullptr)
+			ret_mem_ptr->data[0] = 0;
 		return static_cast<void*>(ret_mem_ptr);
 	}
-	void MAllocator::pool_deallocate(void* ptr, size_t mem_size)
+	void MAllocator::pool_deallocate(void** ptr, size_t mem_size)
 	{
-		char* p = nullptr;
-
+		size_t mem_index = MAllocator::pool_freelist_index(mem_size);
+		MAllocatorRep* tmp_ptr = MAllocator::pool_mem_pool[mem_index];
+		MAllocatorRep* giveback_ptr = static_cast<MAllocatorRep*>(*ptr);
+		// 将归还内容置于顶部   
+		giveback_ptr->next = tmp_ptr;
+		giveback_ptr->data[0] |= __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__;
+		MAllocator::pool_mem_pool[mem_index] = giveback_ptr;
+		*ptr = nullptr;// 将指针指向空 使得在delete后不会被越权访问
 	}
 	void* MAllocator::pool_reallocate()
 	{
 		return nullptr;
 	}
-	void* MAllocator::pool_allocate_array(size_t type_array_size)
-	{
-		char* p = nullptr;
 
-		return p;
-	}
-	void MAllocator::pool_deallocate_array(void* ptr, size_t mem_size)
-	{
-		char* p = nullptr;
-
-	}
 #endif // __MUZI_ALLOCATOR_MOD_POOL__
 
 
--- src/Allocator.h
+++ src/Allocator.h
@@ -25,8 +25,18 @@
 #define __MUZI_ALLOCAOTR_MOD_POOL_ROUNDUP__(x) x >> 4
 // 边界控制大小（对齐内存）
 #define __MUZI_ALLOCAOTR_MOD_POOL_ALIGN__ 8
+// 可以接受的最大申请量
+#define __MUZI_ALLOCAOTR_MOD_POOL_MAX_SPECIFICATION__ 128
+// 内存的边界
+#define __MUZI_ALLOCAOTR_MOD_POOL_MEM_BOARD_FLAG__ 3
 
 #endif // __MUZI_ALLOCATOR_MOD_POOL__
+
+// 为了匹配大小端机器所创立的指针传递方式 x为操作指针，y为需要和指针相加减的值
+#define __MUZI_MEM_BIG_BOARD_ITEARTOR_ADVANCE__(x, y) x - y
+#define __MUZI_MEM_BIG_BOARD_ITEARTOR_BACK__(x, y) x + y
+#define __MUZI_MEM_SMALL_BOARD_ITEARTOR_ADVANCE__(x, y) x + y
+#define __MUZI_MEM_SMALL_BOARD_ITEARTOR_BACK__(x, y) x - y
 
 
 namespace MUZI
@@ -38,26 +48,18 @@ namespace MUZI
 		// 规定每种模式都需要传递一个清理函数到此处
 		using clearMemoryFunction = void(*)(void*);
 		static clearMemoryFunction cmf[__MUZI_ALLOCATOR_MOD_SIZE__];
-		static void* cmf_arg;
-		static void callCMF()
-		{
-			for (int i = 0; i < __MUZI_ALLOCATOR_MOD_SIZE__; ++i)
-			{
-				MAllocator::cmf[i](MAllocator::cmf_arg);
-			}
-		}
+		static void* cmf_arg[__MUZI_ALLOCATOR_MOD_SIZE__];
+		static void callCMF();
+		static void setClearMemoryFunction(clearMemoryFunction fun, void* arg);
 	public:// 公用构造函数
-		MAllocator()
-		{
-			MAllocator::object_num += 1;
-			//atexit(MAllocator::callCMF);
-		}
+		MAllocator();
+		~MAllocator();
 #ifdef __MUZI_ALLOCATOR_MOD_POOL__ // 不同模式下的不同数据结构和对应算法
 	public://数据模型
 		union MAllocatorRep
 		{
-			union MAllocatorRep* next;
-			char data[1];
+			union MAllocatorRep* next;//前四个字节为指针
+			uint32_t data[1];//后四个字节为内存边界标识符
 		};
 	public://数据结构
 		// 不同规格的申请量所规定的数组序列， 内部的指针维持着一个单向链表
@@ -68,18 +70,16 @@ namespace MUZI
 
 	private:
 		// 追加量 调整数据大小上界，并且调整数据为8的边界
-		static size_t pool_RoundUp();// 调整申请内存边界
+		static size_t pool_RoundUp(size_t bytes);// 调整申请内存边界
 		static size_t pool_freelist_index(size_t bytes);// 获取对应freelist的数组下标
-		static void* pool_mem_split();// 内存分片函数
+		//static void* pool_mem_split();// 内存分片函数
 	public:
 		static void* pool_allocate(size_t type_size);// 申请分配内存
-		static void pool_deallocate(void* ptr, size_t mem_size);// 回收内存
+		static void pool_deallocate(void** ptr, size_t mem_size);// 回收内存, 在这里采用传入指针地址的方式，将原指针地址指向空以保证不会越权访问
 		static void* pool_reallocate();// 延长原先申请的内存空间
 		static void* pool_chunk_allocate();// 大块内存分配请求
-		static void* refill();// 充值战备池
-
-		static void* pool_allocate_array(size_t type_array_size);
-		static void pool_deallocate_array(void* ptr, size_t mem_size);
+		static void* pool_refill();// 充值战备池
+		static bool pool_is_possible_mem_board(void* p);//查看当前指针所指向的一个字节是否包含内存边界
 #endif // __MUZI_ALLOCATOR_MOD_POOL__
 
 	};
