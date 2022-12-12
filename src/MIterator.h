0e871e422ef2a39e3adee8a254ef999e3b882f4d
shikoumuzi <1013740747@qq.com>
2022-12-12 23:32:06 +0800

    MSpan基本实现，模板实例化处还需要琢磨

--- src/MIterator.h
+++ src/MIterator.h
@@ -5,17 +5,23 @@ namespace MUZI
 	class MIterator
 	{
 	public:
-		MIterator()
-		{
-
-		}
-		~MIterator()
-		{
-
-		}
-	public:
 		virtual void operator++() = 0;
 		virtual void operator--() = 0;
+		virtual bool operator==(MIterator& item) = 0;
+		inline virtual bool operator!=(MIterator& item)
+		{
+			return !this->operator==(item);
+		}
+		inline virtual bool operator<=(MIterator& item)
+		{
+			return !this->operator>(item);
+		}
+		inline virtual bool operator>=(MIterator& item)
+		{
+			return !this->operator<(item);
+		}
+		virtual bool operator<(MIterator& item) = 0;
+		virtual bool operator>(MIterator& item) = 0;
 	};
 };
 #endif // !__MUZI_MITERATOR_H__
