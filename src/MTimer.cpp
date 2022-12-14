a5619a56182810943accdf6cd9084d904f92c088
shikoumuzi <1013740747@qq.com>
2022-12-14 22:07:42 +0800

    完成计时器框架，但测试存在无法定位的呼叫的问题

--- src/MTimer.cpp
+++ src/MTimer.cpp
@@ -10,7 +10,9 @@ namespace MUZI
 {
 	// 数据结构
 	static class MTimerData* __MUZI_MTIMER_DATA__ = nullptr;
-	static std::mutex* __MUZI_MTIMER_INIT_LOCK__ = new std::mutex;
+	static std::mutex __MUZI_MTIMER_INIT_LOCK__;
+	using __MUZI_MTIMER_MIN_TIME_UNIT_TYPE__ = std::chrono::milliseconds;// 最小时间单位类型
+	using __MUZI_MTIMER_MIN_TIME_UNIT_TYPE_RATIONAL__ = std::milli;// 最小时间单位对应的有理数
 
 	class MTimerData
 	{
@@ -19,6 +21,8 @@ namespace MUZI
 		static void delMTimerData(MTimerData** data);
 	public:
 		static Task& get_free_task();
+		static bool add_task(const Task& task);
+
 		//static inline bool get_exit_flag()
 		//{
 		//	return __MUZI_MTIMER_DATA__->exit_flag;
@@ -43,10 +47,11 @@ namespace MUZI
 	// 任务状态
 	enum MTimerTaskStatusFlag
 	{
-		WORKINGNOW = 0x00000001u,
-		NOWORKINGNOW = 0x00000010u,
-		FREETASK = 0x10000000u,
-		BUSYTASK = 0x01000000u
+		WORKINGNOW = 0x00000001u,// 代表该人物处于非工作状态（暂停或者未启动）
+		NOWORKINGNOW = 0x00000010u,// 代表该任务处于工作状态
+		FREETASK = 0x10000000u,// 代表该任务目前处于没有接手的状态
+		BUSYTASK = 0x01000000u,// 代表该任务处于接手状态
+		ERRORTASK = 0x00011000u
 	};
 
 	struct MTimerTaskStatus
@@ -55,9 +60,11 @@ namespace MUZI
 		std::chrono::steady_clock::time_point task_start_time;// 任务起始时间
 		std::atomic<uint64_t> wake_times;// 被唤醒次数
 		std::atomic<uint64_t> activate_times;// 激活所需要的次数
-		std::unique_lock<std::mutex> task_lock;// 条件变量匹配的锁
+		std::mutex contributing_lock;// 起作用的 
+		//std::unique_lock<std::mutex> task_lock;// 条件变量匹配的锁, 用以传参, 同时该unqiue_lock一旦初始化，立刻上锁
 		std::condition_variable task_cond;// 激活的条件变量，用以激活call_task函数
 		size_t pos;
+		size_t thread_pos;
 	};
 
 	// 定时线程
@@ -68,15 +75,17 @@ namespace MUZI
 		std::thread child_th;
 		// 下面是让main_thread唤醒子线程所需要的函数
 		std::condition_variable* cond;
-		std::unique_lock<std::mutex>* cond_unique_lock;
+		std::mutex* cond_lock;
+		size_t tasks_num;
 	};
 	void __MUZI_MTIMER_CHILD_THREAD_FUN__(void* job_ptr)
 	{
 		struct MTimerChildThread& child_thread = *static_cast<struct MTimerChildThread*>(job_ptr);
 		while (!__MUZI_MTIMER_DATA__->exit_flag)
 		{
+			std::unique_lock<std::mutex> cond_lock(*child_thread.cond_lock);
 			// 子线程 处于等待状态，等待主线程抵达下一时间单元的命令
-			child_thread.cond->wait(*child_thread.cond_unique_lock);
+			child_thread.cond->wait(cond_lock);
 			for (size_t i = child_thread.min_pos; i <= child_thread.max_pos; ++i)
 			{
 				if (__MUZI_MTIMER_DATA__->tasks_status[i].work_status & MTimerTaskStatusFlag::WORKINGNOW)
@@ -96,20 +105,23 @@ namespace MUZI
 	// 计时线程
 	struct MTimerMainThread
 	{
-		std::thread main_th;
-		std::condition_variable cond;
-		std::unique_lock<std::mutex> cond_unique_lock;
-		std::chrono::steady_clock::time_point work_start_time;
-		std::chrono::microseconds timeout_standnrd;
+		std::thread main_th;// 计时线程
+		std::condition_variable cond;// 用以传递计时信息的条件变量
+		std::mutex contributing_lock;// 起作用的锁
+		//std::unique_lock<std::mutex> cond_unique_lock;
+		//// std::unique_lock实际上是封装mutex，所以在初始化的时候必须要导入一个std::mutex进行初始化，否则会报死锁错误
+		////同时该unqiue_lock一旦初始化，立刻上锁
+		std::chrono::steady_clock::time_point work_start_time;// 线程工作时间
+		__MUZI_MTIMER_MIN_TIME_UNIT_TYPE__ timeout_standnrd;// 最小时间单元
 	};
 	void __MUZI_MTIMER_MAIN_THREAD_FUN__(void* job_ptr)
 	{
 		struct MTimerMainThread& main_thread = *static_cast<struct MTimerMainThread*>(job_ptr);
 		main_thread.work_start_time = std::chrono::steady_clock::now();
-		main_thread.cond_unique_lock.lock();
+		static std::unique_lock<std::mutex> main_thread_lock(main_thread.contributing_lock);
 		// 根据最小单位叠加，等到抵达下一个单元的时候向其他线程提醒动作
 		while (((main_thread.cond.
-			wait_for(main_thread.cond_unique_lock, main_thread.timeout_standnrd) == std::cv_status::timeout)
+			wait_for(main_thread_lock, main_thread.timeout_standnrd) == std::cv_status::timeout)
 			|| errno == ETIMEDOUT) && (!__MUZI_MTIMER_DATA__->exit_flag))
 		{
 			main_thread.cond.notify_all();
@@ -117,27 +129,31 @@ namespace MUZI
 	}
 
 	// 静态变量初始化和 函数定义
-	struct MTimerTaskStatus MTimerData::tasks_status[__MUZI_MTIMER_MAX_TASK_SIZE__];
-	struct MTimerChildThread MTimerData::tasks_threads[__MUZI_MTIMER_MAX_THREAD_SIZE__];
+	struct MTimerTaskStatus MTimerData::tasks_status[__MUZI_MTIMER_MAX_TASK_SIZE__] = {};
+	struct MTimerChildThread MTimerData::tasks_threads[__MUZI_MTIMER_MAX_THREAD_SIZE__] = {};
 	struct MTimerMainThread MTimerData::main_thread;
 	bool MTimerData::exit_flag = false;
 	size_t MTimerData::task_size = 0;
 	size_t MTimerData::object_num = 0;
+	Task MTimer::ErrorTask;
+
 
 	MTimerData* MTimerData::getMTimerData()
 	{
-		__MUZI_MTIMER_INIT_LOCK__->lock();
-		if (__MUZI_MTIMER_DATA__ != nullptr)
+		__MUZI_MTIMER_INIT_LOCK__.lock();
+		if (__MUZI_MTIMER_DATA__ == nullptr)
 		{
 			__MUZI_MTIMER_DATA__ = new MTimerData;
 			size_t i = 0;
+			MTimerTaskStatus* tasks_status = nullptr;
 			// 初始化任务
 			for (; i < __MUZI_MTIMER_MAX_TASK_SIZE__; ++i)
 			{
-				__MUZI_MTIMER_DATA__->tasks_status[i].work_status = MTimerTaskStatusFlag::FREETASK;
-				__MUZI_MTIMER_DATA__->tasks_status[i].wake_times = 0;
-				__MUZI_MTIMER_DATA__->tasks_status[i].activate_times = 0;
-				__MUZI_MTIMER_DATA__->tasks_status[i].pos = i;
+				tasks_status = &__MUZI_MTIMER_DATA__->tasks_status[i];
+				tasks_status->work_status = MTimerTaskStatusFlag::FREETASK;
+				tasks_status->wake_times = 0;
+				tasks_status->activate_times = 0;
+				tasks_status->pos = i;
 			}
 			// 初始化主线程
 			static struct MTimerMainThread& main_thread = __MUZI_MTIMER_DATA__->main_thread;
@@ -145,36 +161,33 @@ namespace MUZI
 			// 设定任务开始时间
 			main_thread.work_start_time = std::chrono::steady_clock::now();
 			// 初始化为10微秒作为一个时间单位
-			main_thread.timeout_standnrd = std::chrono::duration<uint64_t, std::micro>(10);
+			main_thread.timeout_standnrd = std::chrono::duration<uint64_t, __MUZI_MTIMER_MIN_TIME_UNIT_TYPE_RATIONAL__>(__MUZI_MTIMER_MIN_TIME_UNIT__);
 			main_thread.main_th = std::move(std::thread(__MUZI_MTIMER_MAIN_THREAD_FUN__, &__MUZI_MTIMER_DATA__->main_thread));
 
 			// 初始化子线程
 			struct MTimerChildThread* child_threads = __MUZI_MTIMER_DATA__->tasks_threads;
 			size_t thread_tasks_num = __MUZI_MTIMER_MAX_TASK_SIZE__ / __MUZI_MTIMER_MAX_THREAD_SIZE__;
 			size_t start_pos = 0, end_pos = thread_tasks_num;
+			size_t j = 0;
 			for (i = 0; i < __MUZI_MTIMER_MAX_THREAD_SIZE__; ++i)
 			{
 				child_threads[i].cond = &main_thread.cond;
-				child_threads[i].cond_unique_lock = &main_thread.cond_unique_lock;
+				//child_threads[i].cond_unique_lock = &main_thread.cond_unique_lock;
+				child_threads[i].cond_lock = &main_thread.contributing_lock;
 				child_threads[i].min_pos = start_pos;
 				child_threads[i].max_pos = end_pos;
-				child_threads[i].child_th = std::move(std::thread(__MUZI_MTIMER_CHILD_THREAD_FUN__, &child_threads[i]));
+				child_threads[i].tasks_num = 0;
+				//child_threads[i].child_th = std::move(std::thread(__MUZI_MTIMER_CHILD_THREAD_FUN__, &child_threads[i]));
+				for (j = start_pos; j <= end_pos; ++j)
+				{
+					__MUZI_MTIMER_DATA__->tasks_status[i].thread_pos = i;
+				}
 				start_pos = end_pos + 1;
 				end_pos += thread_tasks_num;
 			}
-
-			__MUZI_MTIMER_INIT_LOCK__->unlock();
-			delete __MUZI_MTIMER_INIT_LOCK__;
-			__MUZI_MTIMER_DATA__ = nullptr;
-			return __MUZI_MTIMER_DATA__;
 		}
-		else
-		{
-			if (__MUZI_MTIMER_INIT_LOCK__ != nullptr)
-				__MUZI_MTIMER_INIT_LOCK__->unlock();
-
-			return __MUZI_MTIMER_DATA__;
-		}
+		__MUZI_MTIMER_INIT_LOCK__.unlock();
+		return __MUZI_MTIMER_DATA__;
 
 	}
 	void MTimerData::delMTimerData(MTimerData** data)
@@ -195,12 +208,35 @@ namespace MUZI
 				return __MUZI_MTIMER_DATA__->tasks_status[i];
 			}
 		}
+		return MTimer::ErrorTask;
 	}
-
+	// 给线程添加任务，添加该函数的目的是保证没有进行任务的线程不会被创建，只有当有任务的线程才会启动
+	bool MTimerData::add_task(const Task& task_flag)
+	{
+		Task& task = __MUZI_MTIMER_DATA__->tasks_status[task_flag.pos];
+		if (task.work_status & MTimerTaskStatusFlag::WORKINGNOW)// 首先查看是否处于工作状态，确保不重复对一个任务进行操作
+		{
+			return false;
+		}
+		// 确定是一个未工作中的线程
+		MTimerChildThread& tasks_thread = __MUZI_MTIMER_DATA__->tasks_threads[task.thread_pos];
+		task.work_status = MTimerTaskStatusFlag::BUSYTASK | MTimerTaskStatusFlag::WORKINGNOW;
+		task.task_start_time = std::chrono::steady_clock::now();
+		if (tasks_thread.tasks_num == 0)// 如果挂载在该线程上的任务为0，则代表该线程没有人使用过，所以需要创立线程
+		{
+			tasks_thread.child_th = std::move(std::thread(__MUZI_MTIMER_CHILD_THREAD_FUN__, &tasks_thread));
+		}
+		++tasks_thread.tasks_num;
+		return true;
+	}
 
 	// 下面是头文件所看到的函数内容
 	MTimer* MTimer::getMTimer()
 	{
+		MTimer::ErrorTask.work_status = MTimerTaskStatusFlag::ERRORTASK;
+		MTimer::ErrorTask.wake_times = static_cast<uint64_t>(-1);
+		MTimer::ErrorTask.activate_times = static_cast<uint64_t>(-1);
+		MTimer::ErrorTask.pos = static_cast<size_t>(-1);
 		return new MTimer();
 	}
 	void MTimer::delMTimer(MTimer** mtimer)
@@ -222,31 +258,25 @@ namespace MUZI
 			this->data->delMTimerData(&this->data);
 		}
 	}
-	// 转换函数
-	inline uint32_t MTimer::millsecond_2_microsecond(uint32_t millsecond)
-	{
-		return static_cast<uint32_t>(millsecond / 1000);
-	}
-	inline uint32_t MTimer::second_2_microsecond(uint32_t second)
-	{
-		return static_cast<uint32_t>(second / 1000000);
-	}
+
 	// 操作函数
 	const Task& MTimer::set_task(uint32_t microsecond)// 设置定时任务,返回一个标号
 	{
 		Task& ret_task = this->data->get_free_task();
-		ret_task.activate_times = microsecond / __MUZI_MTIMER_MIN_TIME_UNIT__;
-		ret_task.work_status &= MTimerTaskStatusFlag::BUSYTASK | MTimerTaskStatusFlag::NOWORKINGNOW;
-		__MUZI_MTIMER_DATA__->task_size += 1;
+		if (!this->is_error_task(ret_task))
+		{
+			ret_task.activate_times = microsecond / __MUZI_MTIMER_MIN_TIME_UNIT__;
+			ret_task.work_status = MTimerTaskStatusFlag::BUSYTASK | MTimerTaskStatusFlag::NOWORKINGNOW;
+			__MUZI_MTIMER_DATA__->task_size += 1;
+		}
 		return ret_task;
 	}
-	inline const Task& MTimer::start_task(const Task& task_flag)// 开始任务
+	const Task& MTimer::start_task(const Task& task_flag)// 开始任务
 	{
 		//Task& ret_task = __MUZI_MTIMER_DATA__->tasks_status[task_flag.pos];
 		//ret_task.work_status &= MTimerTaskStatusFlag::BUSYTASK | MTimerTaskStatusFlag::WORKINGNOW;
 		//return ret_task;
-		return (__MUZI_MTIMER_DATA__->tasks_status[task_flag.pos].work_status
-			&= MTimerTaskStatusFlag::BUSYTASK | MTimerTaskStatusFlag::WORKINGNOW, task_flag);
+		return (__MUZI_MTIMER_DATA__->add_task(task_flag), task_flag);
 	}
 	const Task& MTimer::stop_task(const Task& task_flag)// 暂停任务
 	{
@@ -256,9 +286,8 @@ namespace MUZI
 	bool MTimer::call_task(const Task& task_flag)// 呼叫定时任务，当当前任务到规定条件时返回对应标识符
 	{
 		MTimerTaskStatus& task = __MUZI_MTIMER_DATA__->tasks_status[task_flag.pos];
-		task.task_lock.lock();
-		task.task_cond.wait(task.task_lock);
-		task.task_lock.unlock();
+		std::unique_lock<std::mutex> task_lock(task.contributing_lock);
+		task.task_cond.wait(task_lock);
 		return true;
 	}
 	int MTimer::del_task(const Task& task_flag)// 取消定时任务,成功返回对应标识符号，失败返回-1
@@ -274,12 +303,20 @@ namespace MUZI
 	{
 		return __MUZI_MTIMER_DATA__->task_size;
 	}
-	inline size_t MTimer::maxsize()// 最大任务数
+	constexpr size_t MTimer::maxsize()// 最大任务数
 	{
 		return __MUZI_MTIMER_MAX_TASK_SIZE__;
 	}
 	inline bool MTimer::is_full()
 	{
 		return __MUZI_MTIMER_MAX_TASK_SIZE__ == __MUZI_MTIMER_DATA__->task_size;
+	}
+	uint64_t MTimer::work_time(const Task& task)
+	{
+		return std::chrono::duration_cast<__MUZI_MTIMER_MIN_TIME_UNIT_TYPE__>(std::chrono::steady_clock::now() - task.task_start_time).count();
+	}
+	inline bool MTimer::is_error_task(const Task& task)
+	{
+		return task.work_status & MTimerTaskStatusFlag::ERRORTASK;
 	}
 };
--- src/MTimer.h
+++ src/MTimer.h
@@ -2,17 +2,20 @@
 #ifndef __MUZI_MTIMER_H__
 #define __MUZI_MTIMER_H__
 #include<stdint.h>
-
+#include<chrono>
 namespace MUZI
 {
 #define __MUZI_MTIMER_MAX_TASK_SIZE__ 1016
 #define __MUZI_MTIMER_MAX_THREAD_SIZE__ 8
 #define __MUZI_MTIMER_TOTAL_THREAD__ __MUZI_MTIMER_MAX_THREAD_SIZE__ + 1
-#define __MUZI_MTIMER_MIN_TIME_UNIT__ 10 /*这里采用的是微妙，所以定义最小时间单位为10微妙*/
+#define __MUZI_MTIMER_MIN_TIME_UNIT__ 1 /*这里采用的是微妙，所以定义最小时间单位为10微妙*/
 	using Task = struct MTimerTaskStatus;
 	using CTask = const Task&;
+	
 	class MTimer
 	{
+	public:
+		static Task ErrorTask;
 	public:
 		using value_type = class MTimerData*;
 	public:
@@ -28,15 +31,77 @@ namespace MUZI
 		uint32_t second_2_microsecond(uint32_t second);
 	public:// 操作函数
 		CTask set_task(uint32_t microsecond);// 设置定时任务,返回一个标号
-		CTask start_task(const Task& task_flag);// 开始任务
-		CTask stop_task(const Task& task_flag);// 暂停任务
-		bool call_task(const Task& task_flag);// 呼叫定时任务，当当前任务到规定条件时返回对应标识符
-		int del_task(const Task& task_flag);// 取消定时任务,成功返回0，失败返回-1
+		CTask start_task(CTask task_flag);// 开始任务
+		CTask stop_task(CTask task_flag);// 暂停任务
+		bool call_task(CTask task_flag);// 呼叫定时任务，当当前任务到规定条件时返回对应标识符
+		int del_task(CTask task_flag);// 取消定时任务,成功返回0，失败返回-1
 	public:// 获取定时器状态
 		size_t size();// 任务数
-		size_t maxsize();// 最大任务数
+		constexpr size_t maxsize();// 最大任务数
 		bool is_full();
-		uint64_t work_time();
+		uint64_t work_time(CTask task);// 返回经历了多少微妙
+		bool is_error_task(CTask task);
+	public:
+		template<typename T1, typename T2 = std::nullptr_t>// 当输入为0nullptr_t类型的时候确认为1
+		uint32_t transition_second(uint32_t orgnal_num)
+		{
+			// 编译器if（if constexptr）不支持短路原则
+			// 首先为秒转换到毫秒和微秒的数量级
+			if constexpr (std::is_same<T1, std::nullptr_t>::value)
+			{
+				if constexpr (std::is_same<T1, T2>::value)
+				{
+					return orgnal_num;
+				}
+				else
+				{
+					if constexpr (std::is_same<T2, std::milli>::value)
+					{
+						return orgnal_num * 1000;
+					}
+					else
+					{
+						if constexpr (std::is_same<T2, std::micro>::value)
+						{
+							return orgnal_num * 1000000;
+						}
+					}
+				}
+
+			}
+			else
+			{
+				// 这里是毫秒到其他妙的转换
+				if constexpr (std::is_same<T1, std::milli>::value)
+				{
+					if constexpr (std::is_same<T2, std::nullptr_t>::value)
+					{
+						return orgnal_num / 1000;
+					}
+					else
+					{
+						if constexpr (std::is_same<T2, std::micro>::value)
+						{
+							return orgnal_num * 1000;
+						}
+					}
+				}
+				else
+				{
+					if constexpr (std::is_same<T2, std::nullopt_t>::value)
+					{
+						return orgnal_num / 1000000;
+					}
+					else
+					{
+						if constexpr (std::is_same<T2, std::milli>::value)
+						{
+							return orgnal_num * 1000;
+						}
+					}
+				}
+			}
+		}
 	private:
 		class MTimerData* data;
 	};
