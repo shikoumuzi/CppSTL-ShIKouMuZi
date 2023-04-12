b542346043bde4a91af3eac49f30b8344b79fc33
shikoumuzi <1013740747@qq.com>
2023-04-12 20:15:32 +0800

    修改mmap的多态实现方式

--- src/MMap.h
+++ src/MMap.h
@@ -3,6 +3,7 @@
 #include<compare>
 namespace MUZI
 {
+	//pair
 	template<typename T>
 	concept __MMap_Pair_First_Ele_Type__ = requires(T x)
 	{
@@ -29,20 +30,35 @@ namespace MUZI
 
 	};
 
-	template<typename T, typename K>
+	//mmap
+	template<typename T>
+	concept __MMAP_TYPE__ = requires(T x)
+	{
+		x.set();
+		x.get();
+		x.inset();
+		x.operator[]();
+		x.data();
+	};
+
+
+	template<typename T, typename K, __MMAP_TYPE__ Map = nullptr_t>
 	class MMap
 	{
-
+	private:
+		Map map;
 	};
 
+	//hashmap
 	template<typename T, typename K>
-	class MHashMap: public MMap
+	class MHashMap
 	{
 
 	};
 
+	//treemap
 	template<__MMap_Pair_First_Ele_Type__  T, typename K>
-	class MTreeMap :public MMap
+	class MTreeMap
 	{
 
 	};
