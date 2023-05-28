706b2e989122f413160370bf0818880c6b06bb2b
shikoumuzi <1013740747@qq.com>
2023-05-28 17:10:09 +0800

    初始化FileIOQueeu

--- src/MFileSystem/FileIOQueue.h
+++ src/MFileSystem/FileIOQueue.h
@@ -2,13 +2,25 @@
 #include"../MBase/MObjectBase.h"
 #include<atomic>
 #include<stdio.h>
+#include"MFile.h"
+#include"MFileInput.h"
+#include"MFileOutput.h"
 namespace MUZI
 {
 	class FileIOQueue
 	{
 	public:
 		FileIOQueue();
+		FileIOQueue(const FileIOQueue&) = delete;
+		FileIOQueue(FileIOQueue&&);
+
+	public:
 		
+		
+
+	public:
+		std::atomic<int> read_index;
+		std::atomic<int> write_index;
 	};
 }
 
