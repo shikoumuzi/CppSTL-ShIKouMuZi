94ff977d9c6fa1f6e5b5647c609db59a0b908bb1
shikoumuzi <1013740747@qq.com>
2023-06-01 19:11:13 +0800

    设置基础属性识别

--- src/MSQLite/MSQLiite.h
+++ src/MSQLite/MSQLiite.h
@@ -3,11 +3,21 @@
 #include<sqlite3.h>
 
 #define __MUZI_MSQLITE_SQL_CODE_SIZE__ 1024
+#define __MUZI_MSQLITE_MAX_ATTRIBUTE_SIZE__ 8
 namespace MUZI
 {
 	// 该类只能绑定一个db数据库
 	class MSQLite
 	{
+	public:
+		enum SQLType
+		{
+			SQL_SELECT = 100,
+			SQL_CREATE,
+			SQL_DELETE,
+			SQL_INSERT
+		};
+		enum __SQLAttributeType__;
 	private:
 		struct __SQL_MESSAGE__;
 		struct __MSQLite_Data__;
@@ -22,8 +32,8 @@ namespace MUZI
 	public:
 		int setMode();
 	public:
-		int createDB();
-		int createTable();
+		int createDB(const char* database_name);
+		int createTable(const char* sql);
 	public:
 		int bindDB();
 	public:
@@ -32,9 +42,16 @@ namespace MUZI
 		sql_id_t registerSQL(const char* sql, int type);
 		sql_type_t getSQLType(sql_id_t sql_id);
 		int driverSQL(sql_id_t sql_id, void* data, size_t data_size, ...);
-
+	public:
+		inline bool isSELECTT(sql_type_t);
+		inline bool isCREATE(sql_type_t);
+		inline bool isDELETE(sql_type_t);
+		inline bool isINSERT(sql_type_t);
+		inline bool isErrorType(sql_type_t);
 	private:
 		size_t __analizeSQL__(); 
+		struct __SQL_MESSAGE__* __checkSQLID__(sql_id_t sql_id, int& err_no);
+
 	private:
 		struct __MSQLite_Data__* m_data;
 	};
--- src/MSQLite/MSQLite.cpp
+++ src/MSQLite/MSQLite.cpp
@@ -4,6 +4,7 @@
 #include<compare>
 #include<boost/filesystem.hpp>
 #include"../MBase/MError.h"
+#include<stdint.h>
 namespace MUZI
 {
 	struct MSQLite::__SQL_MESSAGE__
@@ -13,17 +14,33 @@ namespace MUZI
 		MSQLite::sql_type_t sql_type;
 		sqlite3_stmt* data;
 		size_t attribute_num;
+		char attribute_type[__MUZI_MSQLITE_MAX_ATTRIBUTE_SIZE__];
 	};
 	struct MSQLite::__MSQLite_Data__
 	{
 		struct MSQLite::__SQL_MESSAGE__* sqls;
 		sqlite3* sq3;
+
+	};
+	enum MSQLite::__SQLAttributeType__
+	{
+		INT = 1,
+		DOUBLE,
+		INT64,
+		_NULL,
+		TEXT,
+		TEXT16
 	};
 
 	MSQLite::MSQLite():m_data(new struct MSQLite::__MSQLite_Data__)
 	{
 		boost::filesystem::current_path(boost::filesystem::initial_path<boost::filesystem::path>());
 		this->m_data->sqls = new struct __SQL_MESSAGE__[__MUZI_MSQLITE_SQL_CODE_SIZE__];
+		__SQL_MESSAGE__ sql_message = {-1, 0, nullptr, 0};
+		for (int i = 0; i < __MUZI_MSQLITE_SQL_CODE_SIZE__; ++i)
+		{
+			memcpy(&this->m_data->sqls[i], &sql_message, sizeof(__SQL_MESSAGE__));
+		}
 	}
 	MSQLite::MSQLite(MSQLite&& that):m_data(that.m_data)
 	{
@@ -46,7 +63,7 @@ namespace MUZI
 
 	}
 
-	int MSQLite::createDB()
+	int MSQLite::createDB(const char* database_name)
 	{
 
 
@@ -55,7 +72,7 @@ namespace MUZI
 		return 0;
 	}
 
-	int MSQLite::createTable()
+	int MSQLite::createTable(const char* sql)
 	{
 
 	}
@@ -76,25 +93,85 @@ namespace MUZI
 	}
 	MSQLite::sql_type_t MSQLite::getSQLType(sql_id_t sql_id)
 	{
-
+		int err_no = 0;
+		__SQL_MESSAGE__* sql = this->__checkSQLID__(sql_id, err_no);
+		if (sql == nullptr)
+		{
+			return err_no;
+		}
+		return sql->sql_type;
 	}
 	int MSQLite::driverSQL(sql_id_t sql_id, void* data, size_t data_size, ...)
 	{
-		if (sql_id >= __MUZI_MSQLITE_SQL_CODE_SIZE__)
+		int err_no = 0;
+		__SQL_MESSAGE__* sql = this->__checkSQLID__(sql_id, err_no);
+		if (sql == nullptr)
 		{
-			return MERROR::SQL_ID_ERR;
+			return err_no;
 		}
-		__SQL_MESSAGE__& sql = this->m_data->sqls[sql_id];
-		if (sql.sql_id != sql_id)
-		{
-			return MERROR::SQL_NO_FOUND;
-		}
+		va_list args; // 定义一个va_list类型的变量
+		va_start(args, sql->attribute_num);
 		
+		for (int i = 0; i < sql->attribute_num; ++i)
+		{
+			switch (sql->attribute_type[i])
+			{
+			case __SQLAttributeType__::INT:
+				break;
+			case __SQLAttributeType__::INT64:
+				break;
+				case __SQLAttributeType__::TEXT
+			default:
+				break;
+			}
+		}
+
+
+		va_end(args);
 	}
 
 	size_t MSQLite::__analizeSQL__()
 	{
 		return size_t();
+	}
+
+	struct MSQLite::__SQL_MESSAGE__* MSQLite::__checkSQLID__(sql_id_t sql_id, int& err_no)
+	{
+		if (sql_id >= __MUZI_MSQLITE_SQL_CODE_SIZE__ || sql_id < 0)
+		{
+			err_no = MERROR::SQL_ID_ERR;
+			return nullptr;
+		}
+		__SQL_MESSAGE__* sql = &this->m_data->sqls[sql_id];
+		if (sql->sql_id != sql_id)
+		{
+			err_no = MERROR::SQL_NO_FOUND;
+			return nullptr;
+		}
+		return sql;
+	}
+	bool MSQLite::isSELECTT(sql_type_t sql_type)
+	{
+		return sql_type == SQLType::SQL_SELECT;
+	}
+	bool MSQLite::isCREATE(sql_type_t sql_type)
+	{
+		return sql_type == SQLType::SQL_CREATE;
+	}
+
+	bool MSQLite::isDELETE(sql_type_t sql_type)
+	{
+		return sql_type == SQLType::SQL_DELETE;
+	}
+
+	bool MSQLite::isINSERT(sql_type_t sql_type)
+	{
+		return sql_type == SQLType::SQL_INSERT;
+	}
+
+	bool MSQLite::isErrorType(sql_type_t sql_type)
+	{
+		return sql_type > SQLType::SQL_INSERT || sql_type < SQLType::SQL_SELECT;
 	}
 
 }
