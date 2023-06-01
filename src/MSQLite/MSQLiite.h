3410f44e5a0e88c68c327c04dc122d7c4105b17a
shikoumuzi <1013740747@qq.com>
2023-06-01 18:10:31 +0800

    完善int MSQLite::driverSQL(sql_id_t sql_id, void* data, size_t data_size, ...)

--- src/MSQLite/MSQLiite.h
+++ src/MSQLite/MSQLiite.h
@@ -1,6 +1,8 @@
 #ifndef __MUZI_MSQLITE_H__
 #define __MUZI_MSQLITE_H__
 #include<sqlite3.h>
+
+#define __MUZI_MSQLITE_SQL_CODE_SIZE__ 1024
 namespace MUZI
 {
 	// 该类只能绑定一个db数据库
@@ -13,20 +15,26 @@ namespace MUZI
 		using sql_id_t = size_t;
 		using sql_type_t = size_t;
 	public:
-		MSQLite(const char* sqlite_dir_path = "./sqlite");
+		MSQLite();
 		MSQLite(const MSQLite&) = delete;
 		MSQLite(MSQLite&&);
 		~MSQLite();
 	public:
 		int setMode();
 	public:
+		int createDB();
 		int createTable();
+	public:
+		int bindDB();
 	public:
 		int driverSQL(const char* sql);
 	public:
 		sql_id_t registerSQL(const char* sql, int type);
 		sql_type_t getSQLType(sql_id_t sql_id);
-		int driverSQL(sql_id_t sql_id, ...);
+		int driverSQL(sql_id_t sql_id, void* data, size_t data_size, ...);
+
+	private:
+		size_t __analizeSQL__(); 
 	private:
 		struct __MSQLite_Data__* m_data;
 	};
--- src/MSQLite/MSQLite.cpp
+++ src/MSQLite/MSQLite.cpp
@@ -1,34 +1,101 @@
 #include"MSQLiite.h"
 #include"MRBTree.h"
 #include<string>
+#include<compare>
+#include<boost/filesystem.hpp>
+#include"../MBase/MError.h"
 namespace MUZI
 {
 	struct MSQLite::__SQL_MESSAGE__
 	{
+	public:
 		MSQLite::sql_id_t sql_id;
 		MSQLite::sql_type_t sql_type;
 		sqlite3_stmt* data;
+		size_t attribute_num;
 	};
 	struct MSQLite::__MSQLite_Data__
 	{
-		MRBTree<MSQLite::__SQL_MESSAGE__>* sql_tree;
-		MRBTree<std::string>* table_tree;
+		struct MSQLite::__SQL_MESSAGE__* sqls;
+		sqlite3* sq3;
 	};
 
-
-	MSQLite::MSQLite(const char* sqlite_dir_path = "./sqlite")
+	MSQLite::MSQLite():m_data(new struct MSQLite::__MSQLite_Data__)
 	{
-
+		boost::filesystem::current_path(boost::filesystem::initial_path<boost::filesystem::path>());
+		this->m_data->sqls = new struct __SQL_MESSAGE__[__MUZI_MSQLITE_SQL_CODE_SIZE__];
 	}
-	MSQLite::MSQLite(MSQLite&&)
+	MSQLite::MSQLite(MSQLite&& that):m_data(that.m_data)
 	{
-
+		that.m_data = nullptr;
 	}
 	MSQLite::~MSQLite()
 	{
+		if (this->m_data != nullptr)
+		{
+			if (this->m_data->sqls != nullptr)
+			{
+				delete this->m_data->sqls;
+			}
+			delete this->m_data;
+		}
+	}
+
+	int MSQLite::setMode()
+	{
 
 	}
 
+	int MSQLite::createDB()
+	{
+
+
+
+
+		return 0;
+	}
+
+	int MSQLite::createTable()
+	{
+
+	}
+
+	int MSQLite::bindDB()
+	{
+		return 0;
+	}
+
+	int MSQLite::driverSQL(const char* sql)
+	{
+
+	}
+
+	MSQLite::sql_id_t MSQLite::registerSQL(const char* sql, int type)
+	{
+
+	}
+	MSQLite::sql_type_t MSQLite::getSQLType(sql_id_t sql_id)
+	{
+
+	}
+	int MSQLite::driverSQL(sql_id_t sql_id, void* data, size_t data_size, ...)
+	{
+		if (sql_id >= __MUZI_MSQLITE_SQL_CODE_SIZE__)
+		{
+			return MERROR::SQL_ID_ERR;
+		}
+		__SQL_MESSAGE__& sql = this->m_data->sqls[sql_id];
+		if (sql.sql_id != sql_id)
+		{
+			return MERROR::SQL_NO_FOUND;
+		}
+		
+	}
+
+	size_t MSQLite::__analizeSQL__()
+	{
+		return size_t();
+	}
 
 }
 
