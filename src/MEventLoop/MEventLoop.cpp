0d5681ad565d83f1a1d91f37eaea6622d0f695a5
shikoumuzi <1013740747@qq.com>
2023-11-04 23:06:41 +0800

    完善eventloop 增加原子锁和递归锁的自动切换

--- src/MEventLoop/MEventLoop.cpp
+++ src/MEventLoop/MEventLoop.cpp
@@ -4,7 +4,8 @@ namespace MUZI::_event
 {
 	MEventLoop::MEventLoop():
 		m_thread_id(std::this_thread::get_id()),
-		m_work_flag(false)
+		m_work_flag(false),
+		m_atomic_mode(false)
 	{
 
 	}
@@ -18,59 +19,89 @@ namespace MUZI::_event
 
 	void MEventLoop::run()
 	{
+
+		MTime start_time = std::chrono::system_clock::now();
+		int event_ctrl_times = 0;
+
 		while (this->m_work_flag)
 		{
-			std::lock_guard glock(this->m_rmutex);
-			MEvent top_event = this->m_event_queue.top();
-			switch (top_event.m_event_type)
+			
+			if (this->m_event_queue.size() > 0)
 			{
-			case MEvent::EVENT_TYPE::MOUSE:
+				// 当高于一定频率的事件触发时，则调用原子锁，低于一定频率时，则触发递归锁， 默认为递归锁
+				if (this->m_atomic_mode)
+				{
+					this->m_amutex.lock();
+					MEvent top_event = this->m_event_queue.top();
+					this->ctrlEvent(top_event);
+					this->m_event_queue.pop();
+					this->m_amutex.unlock();
+
+				}
+				else
+				{
+					this->m_rmutex.lock();
+					MEvent top_event = this->m_event_queue.top();
+					this->ctrlEvent(top_event);
+					this->m_event_queue.pop();
+					this->m_rmutex.unlock();
+
+				}
+				
+			}
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+			
+		}
+	}
+	void MEventLoop::ctrlEvent(const MEvent& event)
+	{
+		switch (event.m_event_type)
+		{
+		case MEvent::EVENT_TYPE::MOUSE:
+		{
+			MEvent::MouseEventMsg* event_msg = static_cast<MEvent::MouseEventMsg*>(*event.m_event_msg.get());
+			switch (event_msg->event_type)
 			{
-				MEvent::MouseEventMsg* event_msg = static_cast<MEvent::MouseEventMsg*>(*top_event.m_event_msg.get());
-				switch (event_msg->event_type)
-				{
-				case MEvent::MOUSE_TYPE::CLICK:
-				{
-					break;
-				}
-				case MEvent::MOUSE_TYPE::DOUBLE_CLICK:
-				{
-					break;
-				}
-				case MEvent::MOUSE_TYPE::DROP:
-				{
-					break;
-				}
-				case MEvent::MOUSE_TYPE::HOLD:
-				{
-					break;
-				}
-				case MEvent::MOUSE_TYPE::RELEASE:
-				{
-					break;
-				}
-				default:
-					break;
-				}
+			case MEvent::MOUSE_TYPE::CLICK:
+			{
 				break;
 			}
-			case MEvent::EVENT_TYPE::KEY_BOARD:
+			case MEvent::MOUSE_TYPE::DOUBLE_CLICK:
 			{
-				MEvent::KeyBoardEventMsg* event_msg = static_cast<MEvent::KeyBoardEventMsg*>(*top_event.m_event_msg.get());
-
 				break;
 			}
-			case MEvent::EVENT_TYPE::SIGNAL_TRIGGER:
+			case MEvent::MOUSE_TYPE::DROP:
 			{
-				MEvent::SignalTriggerMsg* event_msg = static_cast<MEvent::SignalTriggerMsg*>(*top_event.m_event_msg.get());
-
+				break;
+			}
+			case MEvent::MOUSE_TYPE::HOLD:
+			{
+				break;
+			}
+			case MEvent::MOUSE_TYPE::RELEASE:
+			{
 				break;
 			}
 			default:
 				break;
 			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(10));
-			
+			break;
+		}
+		case MEvent::EVENT_TYPE::KEY_BOARD:
+		{
+			MEvent::KeyBoardEventMsg* event_msg = static_cast<MEvent::KeyBoardEventMsg*>(*event.m_event_msg.get());
+
+			break;
+		}
+		case MEvent::EVENT_TYPE::SIGNAL_TRIGGER:
+		{
+			MEvent::SignalTriggerMsg* event_msg = static_cast<MEvent::SignalTriggerMsg*>(*event.m_event_msg.get());
+
+			break;
+		}
+		default:
+			break;
 		}
 	}
 }
--- src/MEventLoop/MEventLoop.h
+++ src/MEventLoop/MEventLoop.h
@@ -6,7 +6,7 @@
 #include<thread>
 #include<queue>
 #include<mutex>
-
+#include"MSTL/h/MAtomicLock.h"
 namespace MUZI::_event
 {
 	class MEventLoop
@@ -15,6 +15,7 @@ namespace MUZI::_event
 		using MEventQueue = std::priority_queue<MEvent>;
 		using MThreadId = std::thread::id;
 		using MRMutex = std::recursive_mutex;
+		using MTime = std::chrono::system_clock::time_point;
 
 	public:
 		MEventLoop();
@@ -22,9 +23,13 @@ namespace MUZI::_event
 		void push_back(const MEvent& event);
 		void run();
 	private:
+		void ctrlEvent(const MEvent& event);
+	private:
 		MThreadId m_thread_id;
 		MEventQueue m_event_queue;
 		MRMutex m_rmutex;
+		MAtomicLock m_amutex;
+		bool m_atomic_mode;
 		bool m_work_flag;
 
 	};
