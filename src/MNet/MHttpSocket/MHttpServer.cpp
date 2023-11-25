5ed05e67e01f159d4fa127afec85ede07dd29347
shikoumuzi <1013740747@qq.com>
2023-11-25 20:34:52 +0800

    完成http Content的解析

--- src/MNet/MHttpSocket/MHttpServer.cpp
+++ src/MNet/MHttpSocket/MHttpServer.cpp
@@ -1,10 +1,20 @@
 #include"MHttpServer.h"
+#include<map>
+#include<rapidjson/document.h>
+#include<rapidjson/stringbuffer.h>
+#include<rapidjson/writer.h>
+
 namespace MUZI::net::http
 {
 	class HttpConnectionData
 	{
 	public:
-		HttpConnectionData(TCPSocket& socket, size_t buffer_size = __MUZI_MHTTPSERVER_BUFFER_SIZE__, size_t time_out = __MUZI_MHTTPSERVER_TIMEOUT_VALUE__) :
+		HttpConnectionData(
+			class MHttpServer* parent,
+			TCPSocket& socket,
+			size_t buffer_size = __MUZI_MHTTPSERVER_BUFFER_SIZE__,
+			size_t time_out = __MUZI_MHTTPSERVER_TIMEOUT_VALUE__) :
+			parent(parent),
 			m_socket(std::move(socket)),
 			m_buffer(buffer_size),
 			m_timer(this->m_socket.get_executor(), std::chrono::seconds(time_out)) // 设置超时时间，如果超过该值还没处理完http则返回
@@ -13,28 +23,36 @@ namespace MUZI::net::http
 		TCPSocket m_socket;
 		boost::beast::flat_buffer m_buffer;
 		boost::beast::http::request<boost::beast::http::dynamic_body> m_request; // 动态包体
-		boost::beast::http::response<boost::beast::http::dynamic_body> m_reponse;
+		boost::beast::http::response<boost::beast::http::dynamic_body> m_response;
 		boost::beast::net::steady_timer m_timer;
+		class MHttpServer* parent;
 	};
 
 	class HttpServerData
 	{
 	public:
-
+		std::map<String, MHttpServer::FilePath> m_file_mapping;
 	};
 
-	MHttpServer::MHttpServer()
+	MHttpServer::MHttpServer() :
+		m_data(nullptr)
 	{
 	}
 	MHttpServer::HttpConnection::HttpConnection(
-		TCPSocket& socket, 
-		size_t buffer_size = __MUZI_MHTTPSERVER_BUFFER_SIZE__, 
-		size_t time_out = __MUZI_MHTTPSERVER_TIMEOUT_VALUE__):
-		m_data(new HttpConnectionData(socket, buffer_size, time_out))
+		class MHttpServer* parent,
+		TCPSocket& socket,
+		size_t buffer_size = __MUZI_MHTTPSERVER_BUFFER_SIZE__,
+		size_t time_out = __MUZI_MHTTPSERVER_TIMEOUT_VALUE__) :
+		m_data(new HttpConnectionData(parent, socket, buffer_size, time_out))
 	{
 	}
 	MHttpServer::HttpConnection::~HttpConnection()
 	{
+		if (this->m_data != nullptr)
+		{
+			delete this->m_data;
+			this->m_data = nullptr;
+		}
 	}
 	void MHttpServer::HttpConnection::start()
 	{
@@ -42,6 +60,14 @@ namespace MUZI::net::http
 	void MHttpServer::HttpConnection::readRequest()
 	{
 		auto self = shared_from_this();
+		boost::beast::http::async_read(this->m_data->m_socket, this->m_data->m_buffer, this->m_data->m_request,
+			[self](const EC& ec, std::size_t byte_transferred)
+			{
+				if (!ec)
+				{
+					self->processRequest();
+				}
+			});
 	}
 	void MHttpServer::HttpConnection::checkDeadline()
 	{
@@ -55,5 +81,115 @@ namespace MUZI::net::http
 					self->m_data->m_socket.close();
 				}
 			});
+	}
+	void MHttpServer::HttpConnection::processRequest()
+	{
+		this->m_data->m_response.version(this->m_data->m_request.version());
+		this->m_data->m_response.keep_alive(false); // http协议为短链接
+		switch (this->m_data->m_request.method())
+		{
+		case HttpVerb::get:
+		{
+			this->m_data->m_response.result(HttpStatus::ok);
+			this->m_data->m_response.set(HttpField::server, "Beast");
+			this->createGetResponse();
+			break;
+		}
+		case HttpVerb::post:
+		{
+			this->m_data->m_response.result(HttpStatus::ok);
+			this->m_data->m_response.set(HttpField::server, "Beast");
+			break;
+		}
+		case HttpVerb::patch:
+			break;
+		case HttpVerb::delete_:
+			break;
+		default:
+			// 如果都不匹配
+			this->m_data->m_response.result(HttpStatus::bad_request);
+			this->m_data->m_response.set(HttpField::content_type, "text/plain");
+			boost::beast::ostream(this->m_data->m_response.body())
+				<< "Invalid request-method '"
+				<< this->m_data->m_request.method_string() << "'";
+			break;
+		}
+
+		this->writeResponse();
+	}
+	void MHttpServer::HttpConnection::createGetResponse()
+	{
+		// 查看请求路径， 假设/count表示请求了多少次
+		//if (this->m_data->m_request.target() == "/count")
+		//{
+		//	this->m_data->m_reponse.set(HttpField::content_type, "text/html");
+		//	boost::beast::ostream(this->m_data->m_reponse.body())
+		//		<< "Invalid request-method '"
+		//		<< this->m_data->m_request.method_string() << "'";
+		//}
+		// 查看请求路径
+		auto result_iter = this->m_data->parent->m_data->m_file_mapping.find(String(this->m_data->m_request.target()));
+		if (result_iter == this->m_data->parent->m_data->m_file_mapping.end())
+		{
+			this->m_data->m_response.result(HttpStatus::not_found);
+			this->m_data->m_response.set(HttpField::content_type, "text/plain");
+			boost::beast::ostream(this->m_data->m_response.body())
+				<< "File not found\r\n";
+			return;
+		}
+		String mail_body;
+		boost::filesystem::load_string_file(result_iter->second.c_str(), mail_body);
+		boost::beast::ostream(this->m_data->m_response.body()) << mail_body;
+	}
+	void MHttpServer::HttpConnection::createPostResponse()
+	{
+		auto result_iter = this->m_data->parent->m_data->m_file_mapping.find(String(this->m_data->m_request.target()));
+		if (result_iter == this->m_data->parent->m_data->m_file_mapping.end())
+		{
+			this->m_data->m_response.result(HttpStatus::not_found);
+			this->m_data->m_response.set(HttpField::content_type, "text/plain");
+			boost::beast::ostream(this->m_data->m_response.body())
+				<< "File not found\r\n";
+			return;
+		}
+		auto& body = this->m_data->m_request.body();
+		auto body_str = boost::beast::buffers_to_string(body.data());
+		this->m_data->m_response.set(HttpField::content_type, "text/json");
+		rapidjson::Document doc;
+		rapidjson::StringBuffer response_body_buff;
+		rapidjson::Writer<rapidjson::StringBuffer> json_writer(response_body_buff);
+		doc.Parse(body_str.c_str());
+
+		if (doc.HasParseError())
+		{
+			MLog::w("MHttpServer::HttpConnection::createPostResponse", "Failed to parse json data");
+
+			json_writer.StartObject();
+			json_writer.Key("error");
+			json_writer.Int(1001);
+			json_writer.EndObject();
+			boost::beast::ostream(this->m_data->m_response.body()) << response_body_buff.GetString();
+
+			return;
+		}
+
+		String mail_body;
+		boost::filesystem::load_string_file(result_iter->second.c_str(), mail_body);
+		boost::beast::ostream(this->m_data->m_response.body()) << mail_body;
+	}
+	void MHttpServer::HttpConnection::writeResponse()
+	{
+		auto self = shared_from_this();
+		this->m_data->m_response.content_length(this->m_data->m_response.body().size());
+		boost::beast::http::async_write(
+			this->m_data->m_socket,
+			this->m_data->m_response,
+			[self](const EC& ec, size_t bytes_transafered)
+			{
+				// 只关闭发送端，尽量不要全关，因为有四次挥手的存在，若客户端未响应，而服务器全部关闭时会存在僵尸连接，
+				self->m_data->m_socket.shutdown(TCPSocket::shutdown_send);
+				self->m_data->m_timer.cancel();
+			}
+		);
 	}
 }
--- src/MNet/MHttpSocket/MHttpServer.h
+++ src/MNet/MHttpSocket/MHttpServer.h
@@ -9,6 +9,11 @@
 #include<memory>
 #include"MNet/MNetBase.h"
 #include<boost/beast.hpp>
+#include<boost/filesystem.hpp>
+#include<iostream>
+#include<fstream>
+#include<boost/filesystem/string_file.hpp>
+
 #define __MUZI_MHTTPSERVER_BUFFER_SIZE__ 8192
 #define __MUZI_MHTTPSERVER_TIMEOUT_VALUE__ 60
 
@@ -17,16 +22,29 @@ namespace MUZI::net::http
 	class MHttpServer
 	{
 	public:
-		class HttpConnection: public std::enable_shared_from_this<HttpConnection>
+		friend class HttpConnection;
+	public:
+		using FilePath = boost::filesystem::path;
+
+	public:
+		class HttpConnection : public std::enable_shared_from_this<HttpConnection>
 		{
 		public:
-			HttpConnection(TCPSocket& socket, size_t buffer_size = __MUZI_MHTTPSERVER_BUFFER_SIZE__, size_t time_out = __MUZI_MHTTPSERVER_TIMEOUT_VALUE__);
+			using HttpVerb = boost::beast::http::verb;
+			using HttpStatus = boost::beast::http::status;
+			using HttpField = boost::beast::http::field;
+		public:
+			HttpConnection(class MHttpServer* parent, TCPSocket& socket, size_t buffer_size = __MUZI_MHTTPSERVER_BUFFER_SIZE__, size_t time_out = __MUZI_MHTTPSERVER_TIMEOUT_VALUE__);
 			~HttpConnection();
 		public:
 			void start();
 		public:
 			void readRequest();
 			void checkDeadline();
+			void processRequest();
+			void createGetResponse();
+			void createPostResponse();
+			void writeResponse();
 		public:
 			class HttpConnectionData* m_data;
 		};
