1b2ce1934e63837a9724d538211625bba0e9230b
shikoumuzi <1013740747@qq.com>
2024-05-26 13:55:45 +0800

    fix bug 1.修复if constexpr bug

--- src/MSTL/include/MOptional.h
+++ src/MSTL/include/MOptional.h
@@ -2,19 +2,20 @@
 #define __MUZI_MOPTIONAL_H__
 #include<optional>
 #include<assert.h>
-#include "MBase/MObjectBase.h"
+//#include "MBase/MObjectBase.h"
 namespace MUZI {
+	using __MDefaultTypeDefine__ = int;
 	template<typename T = __MDefaultTypeDefine__>
 	class MOptional {
 	public:
-		using  value_type = std::conditional<std::is_reference<T>::value, T, std::reference_wrapper<T>>::type;
+		using  value_type = std::conditional<!std::is_reference<T>::value, T, std::remove_reference_t<T>*>::type;
 	private:
 		struct __NontrivialDummyType__ {
 			constexpr __NontrivialDummyType__() noexcept {
 				// This default constructor is user-provided to avoid zero-initialization when objects are value-initialized.
 			}
 		};
-		struct MOptionalBase {
+		struct __MOptionalBase__ {
 			union Data
 			{
 				__NontrivialDummyType__ dummy;
@@ -23,6 +24,7 @@ namespace MUZI {
 			bool has_value_flag;
 		};
 	public:
+		// 做成一个标志常量
 		struct NoneOpt { // no-value state indicator
 			struct _Tag {};
 			constexpr explicit NoneOpt(_Tag) {}
@@ -36,27 +38,44 @@ namespace MUZI {
 			return MOptional<T>(InPlacement(), args...);
 		}
 		static MOptional<T> makeNone() {
-			return MOptional<T>(NoneOpt());
+			return MOptional<T>(NoneOpt(NoneOpt::_Tag()));
+		}
+		template<typename ...Args>
+		static value_type getPoint(value_type value, Args ... args) {
+			if (std::is_pointer<value_type>::value) {
+				return value;
+			}
 		}
 	private:
-		MOptionalBase m_base;
+		__MOptionalBase__ m_base;
 
 	public:
-		static NullOpt None;
+		static constexpr NoneOpt None = NoneOpt(NoneOpt::_Tag());
 
 	public:
-		MOptional() {}
-		explicit MOptional(NoneOpt) {}
+		MOptional() : MOptional(NoneOpt(NoneOpt::_Tag())) {}
+		explicit MOptional(NoneOpt) {
+			this->m_base.has_value_flag = false;
+			new(&this->m_base.data.dummy) __NontrivialDummyType__();
+		}
 		template<typename... Args>
-		MOptional(InPlacement, Args ... args) {}
+		MOptional(InPlacement, Args ... args) {
+			this->m_base.has_value_flag = true;
+			if constexpr (!std::is_reference<T>::value) {
+				new(&this->m_base.data.value) value_type(args...);
+			}
+			else {
+				this->m_base.data.value = this->getPoint(args...);
+			}
+		}
 	public:
 		T unwrap() {
 			assert(this->m_base->has_value_flag == false);
-			constexpr if (!std::is_reference<T>::value) {
-				return static_cast<T>(this->m_base.data);
+			if constexpr (!std::is_reference<T>::value) {
+				return static_cast<value_type>(this->m_base.data);
 			}
-			else {
-				return static_cast<T>(this->m_base.data).get();
+			else {// 如果是引用那就退化成指针
+				return *static_cast<value_type>(this->m_base.data);
 			}
 		}
 
